0f00ccd98076e9619ba6c62b5c688a4e8c39c709
Seungjun Song <gunmagician@naver.com>
2025-08-07 14:36:11 +0900

    flexible

--- Study/backend/sts3.9-bundle/workspace/JavaSELabs/src/workshop/oop/flexible/MereClerk.java
+++ Study/backend/sts3.9-bundle/workspace/JavaSELabs/src/workshop/oop/flexible/MereClerk.java
@@ -1,8 +1,6 @@
 package workshop.oop.flexible;
 /* 평사원 클래스 */
 public class MereClerk extends Employee {
-    String name;
-    double salary;
     
     public MereClerk (String name, double salary) {
         this.name = name;
