a6711fd9bef06ae797cca55bbb912a1a80fcd8db
Seungjun Song <gunmagician@naver.com>
2025-08-07 15:45:16 +0900

    Update FlexibleCompanyDemo.java

--- Study/backend/sts3.9-bundle/workspace/JavaSELabs/src/workshop/oop/flexible/FlexibleCompanyDemo.java
+++ Study/backend/sts3.9-bundle/workspace/JavaSELabs/src/workshop/oop/flexible/FlexibleCompanyDemo.java
@@ -22,22 +22,22 @@ public class FlexibleCompanyDemo {
         Manager itMgr2 = (Manager)itMgr;
         System.out.println(itMgr2.getDept());
         		
-        System.out.println("현재 월급입니다.");
         
         //Heterogeneous Collection Employee 타입의 배열의 선언하기
         //emps 변수는 Employee[] 타입, emps[0]는 Employee타입
-        Employee[] emps = new Employee[3];
+        Employee[] emps = new Employee[4];
         emps[0] = new Manager("길동", 250, "IT");
         emps[1] = new MereClerk("철수", 100);
         emps[2] = new MereClerk("영희", 100);
         emps[3] = new Manager("둘리", 200, "HR");
         
-        System.out.println(mereClerk1.getName() + "의 현재 월급은 " + mereClerk1.getSalary() + " 만원 입니다.");
-        System.out.println(mereClerk2.getName() + "의 현재 월급은 " + mereClerk2.getSalary() + " 만원 입니다.");
-        System.out.println(manager.getName() + "의 현재 월급은 " + manager.getSalary() + " 만원 입니다.");
-
-        System.out.println("");
+        for (Employee emp:emps) {
+        	System.out.println("현재 월급입니다.");
+        	System.out.println(emp.getName() + "의 현재 월급은 " + emp.getSalary() + " 만원 입니다.");
+//        	emp.manageSlary(10);
+        }
         
+        System.out.println("");
         
         System.out.println("올린 후의 월급입니다.");
         
