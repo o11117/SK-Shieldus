bcdef46b75049de9cbd04a8ff16339d7a0723e4a
Seungjun Song <gunmagician@naver.com>
2025-08-07 13:29:24 +0900

    generics

--- Study/backend/sts3.9-bundle/workspace/JavaSEProject/src/chap09/ListExample.java
+++ Study/backend/sts3.9-bundle/workspace/JavaSEProject/src/chap09/ListExample.java
@@ -21,7 +21,9 @@ public class ListExample {
 
         // 순서 유지 한다.
         // 데이터 추가 가능하다.
-        List list = new ArrayList();
+        List list = new ArrayList(); //부모 list = new 자식();
+        ArrayList list2 = new ArrayList(); //일반적
+        //add(Object)
         list.add( "one" );
         list.add( "second" );
         list.add( "3rd" );
@@ -31,9 +33,26 @@ public class ListExample {
         list.add( new Integer(4) );
         System.out.println( list );
 
+        //for loop
+//        for(int i=0; i<list.size(); i++) {
+//        	//get(index) 꺼내기 Object -> String으로 casting
+//        	String value = (String)list.get(i);
+//        	System.out.println(value);
+//        }
+        
+
         list.remove( "second" );
         list.remove( new Float(5.0F) );
         System.out.println( list );
 
+        //Generics Java5
+        List<String> strList = new ArrayList<>();
+        strList.add("Hello");
+        strList.add(new String("Collection"));
+        strList.add("Hello");
+        
+        for (String value : strList) {
+			System.out.println(value);
+		}
     }
 }
