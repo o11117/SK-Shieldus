51169b3980f538f35bbe07de4fee56312a52fdae
Seungjun Song <gunmagician@naver.com>
2025-08-08 08:53:40 +0900

    Java HW2

--- Study/backend/sts3.9-bundle/workspace/JavaSELabs/src/mylab/library/entity/Book.java
+++ Study/backend/sts3.9-bundle/workspace/JavaSELabs/src/mylab/library/entity/Book.java
@@ -1,5 +1,72 @@
 package mylab.library.entity;
 
 public class Book extends Library {
+	private String title;
+	private String author;
+	private String isbn;
+	private int publishYear;
+	private boolean isAvailable;
+	
+	public Book() {
+	}
 
+	public Book(String title, String author, String isbn, int publishYear) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
+		this.publishYear = publishYear;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public int getPublishYear() {
+		return publishYear;
+	}
+
+	public void setPublishYear(int publishYear) {
+		this.publishYear = publishYear;
+	}
+
+	public boolean isAvailable() {
+		return isAvailable;
+	}
+	
+	public boolean checkOut() {
+		
+	}
+	
+	public void returnBook() {
+		
+	}
+	
+	public String toString() {
+		
+	}
+
+	
+	
+	
 }
