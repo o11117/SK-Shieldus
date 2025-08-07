9f017410b5de9091ca2b243aa69f67429271ce01
Seungjun Song <gunmagician@naver.com>
2025-08-07 16:02:56 +0900

    override

--- Study/backend/sts3.9-bundle/workspace/JavaSELabs/src/workshop/oop/flexible/MereClerk.java
+++ Study/backend/sts3.9-bundle/workspace/JavaSELabs/src/workshop/oop/flexible/MereClerk.java
@@ -8,6 +8,9 @@ public class MereClerk extends Employee {
     	super(name,salary);
     }
     
+    //오버라이딩 규칙은 부모가 상속해준 메소드의 Signature가 반드시 같아야한다.
+    //@Override 어노테이션은 오버라이딩 규칙을 컴파일타임에 체크해준다.
+    @Override
     public void manageSalary(double rate) {
         salary = salary+ salary*(rate/100);
     }
