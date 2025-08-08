c9b8a2493dd6661642263fc4c9947aa5436d1a60
Seungjun Song <gunmagician@naver.com>
2025-08-08 09:00:25 +0900

    Update Library.java

--- Study/backend/sts3.9-bundle/workspace/JavaSELabs/src/mylab/library/entity/Library.java
+++ Study/backend/sts3.9-bundle/workspace/JavaSELabs/src/mylab/library/entity/Library.java
@@ -1,5 +1,9 @@
 package mylab.library.entity;
 
-public class Library {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Library {
+	private String name;
+	List<Book> list = new ArrayList<>();
 }
