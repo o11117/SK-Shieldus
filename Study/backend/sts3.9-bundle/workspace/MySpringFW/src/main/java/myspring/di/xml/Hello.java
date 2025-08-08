bf327a1bc1ddbf3f9584a446995f596f2430c3d7
Seungjun Song <gunmagician@naver.com>
2025-08-08 16:27:40 +0900

    container

--- Study/backend/sts3.9-bundle/workspace/MySpringFW/src/main/java/myspring/di/xml/Hello.java
+++ Study/backend/sts3.9-bundle/workspace/MySpringFW/src/main/java/myspring/di/xml/Hello.java
@@ -26,12 +26,12 @@ public class Hello {
 	}
 
 	public void setName(String name) {
-		System.out.println(this.getClass().getName() + "setName() 호출된" + name);
+		System.out.println(this.getClass().getName() + "setName() 호출됨 " + name);
 		this.name = name;
 	}
 
 	public void setPrinter(Printer printer) {
-		System.out.println(this.getClass().getName() + "setPrinter() 호출된" + printer.getClass().getName());
+		System.out.println(this.getClass().getName() + "setPrinter() 호출됨 " + printer.getClass().getName());
 		this.printer = printer;
 	}
 
