6953a47be825864576b04c9f23e0995d1162fac5
Seungjun Song <gunmagician@naver.com>
2025-08-08 16:05:10 +0900

    Update HelloJUnit5Test.java

--- Study/backend/sts3.9-bundle/workspace/MySpringFW/src/test/java/myspring/di/xml/HelloJUnit5Test.java
+++ Study/backend/sts3.9-bundle/workspace/MySpringFW/src/test/java/myspring/di/xml/HelloJUnit5Test.java
@@ -1,12 +1,15 @@
 package myspring.di.xml;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class HelloJUnit5Test {
 
 	@Test
 	void helloBean() {
-		
+		//1. Container 객체 생성
+		ApplicationContext context = new GenericXmlApplicationContext("classpath:spring-beans.xml");
 	}
 	
 }
