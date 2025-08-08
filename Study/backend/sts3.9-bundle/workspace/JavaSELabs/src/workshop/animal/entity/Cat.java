1eaccb7949681ee80f7b3ddd55759eb5a16269b8
Seungjun Song <gunmagician@naver.com>
2025-08-08 09:39:07 +0900

    Create Cat.java

---
+++ Study/backend/sts3.9-bundle/workspace/JavaSELabs/src/workshop/animal/entity/Cat.java
@@ -0,0 +1,27 @@
+package workshop.animal.entity;
+
+public class Cat extends Animal implements Pet {
+	private String name;
+
+	@Override
+	public String getName() {
+		return null;
+	}
+
+	@Override
+	public void setName(String name) {
+		
+	}
+
+	@Override
+	public void play() {
+		
+	}
+
+	@Override
+	public void eat() {
+		
+	}
+	
+	
+}
