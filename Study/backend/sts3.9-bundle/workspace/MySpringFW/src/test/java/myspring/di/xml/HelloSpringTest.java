528e7aac0389a75450563b7fa87610be2e43eef2
Seungjun Song <gunmagician@naver.com>
2025-08-11 09:45:16 +0900

    Update HelloSpringTest.java

--- Study/backend/sts3.9-bundle/workspace/MySpringFW/src/test/java/myspring/di/xml/HelloSpringTest.java
+++ Study/backend/sts3.9-bundle/workspace/MySpringFW/src/test/java/myspring/di/xml/HelloSpringTest.java
@@ -1,11 +1,21 @@
 package myspring.di.xml;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+//static import
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:spring-beans.xml")
 public class HelloSpringTest {
-
+	@Autowired
+	Hello hello;
+	
+	@Test
+	void helloBeanByConstructor() {
+//		assertEquals(expected, hello.sayHello());
+		System.out.println(hello.sayHello());
+	}
 }
