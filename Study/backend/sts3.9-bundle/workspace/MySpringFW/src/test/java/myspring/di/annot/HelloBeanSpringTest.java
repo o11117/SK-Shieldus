695061183dbb92feaf4c74489ad3957cdfa8e7cd
Seungjun Song <gunmagician@naver.com>
2025-08-11 13:30:13 +0900

    Update HelloBeanSpringTest.java

--- Study/backend/sts3.9-bundle/workspace/MySpringFW/src/test/java/myspring/di/annot/HelloBeanSpringTest.java
+++ Study/backend/sts3.9-bundle/workspace/MySpringFW/src/test/java/myspring/di/annot/HelloBeanSpringTest.java
@@ -1,6 +1,8 @@
 package myspring.di.annot;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +19,17 @@ public class HelloBeanSpringTest {
 	@Qualifier("stringPrinterBean")
 	PrinterBean printer;
 	
+	//전략2의 constructor injection 설정을 테스트
 	@Test
+	void helloBeanConstructor() {
+		assertEquals("Hello 생성자어노테이션", hello.sayHello());
+		hello.print();
+		assertEquals("Hello 생성자어노테이션", printer.toString());
+	
+	}
+	
+	//전략2의 setter injection 설정을 테스트
+	@Test @Disabled
 	void helloBean() {
 		assertEquals("Hello 어노테이션", hello.sayHello());
 		
