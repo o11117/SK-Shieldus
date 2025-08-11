2229e26961353a3f390a71eed72058fb5f4f3c50
Seungjun Song <gunmagician@naver.com>
2025-08-11 15:58:40 +0900

    property

---
+++ Study/backend/sts3.9-bundle/workspace/MySpringFW/src/test/java/myspring/di/annot/config/HelloBeanConfigTest.java
@@ -0,0 +1,16 @@
+package myspring.di.annot.config;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(
+		classes = HelloBeanConfig.class,
+		loader=AnnotationConfigContextLoader.class)
+//@Configuration을 위한 특별한 컨테이너 클래스인 AnnotationConfigApplicationContext 객체를 로딩해주는 역할
+public class HelloBeanConfigTest {
+	
+	
+}
