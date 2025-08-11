b54500979dd7d85f84eb29f93f6b122fe0d22232
Seungjun Song <gunmagician@naver.com>
2025-08-11 09:49:19 +0900

    quilifier

--- Study/backend/sts3.9-bundle/workspace/MySpringFW/src/test/java/myspring/di/xml/HelloSpringTest.java
+++ Study/backend/sts3.9-bundle/workspace/MySpringFW/src/test/java/myspring/di/xml/HelloSpringTest.java
@@ -1,16 +1,17 @@
 package myspring.di.xml;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-//static import
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:spring-beans.xml")
 public class HelloSpringTest {
 	@Autowired
+	@Qualifier("helloC")
 	Hello hello;
 	
 	@Test
