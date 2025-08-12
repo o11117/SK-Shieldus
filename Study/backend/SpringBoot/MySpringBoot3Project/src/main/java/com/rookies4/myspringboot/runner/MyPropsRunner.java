7322663f2f7c2eff07f86cbbeda7a8b9a9fa0b49
Seungjun Song <gunmagician@naver.com>
2025-08-12 16:59:57 +0900

    runner

--- Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/runner/MyPropsRunner.java
+++ Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/runner/MyPropsRunner.java
@@ -5,6 +5,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.function.Consumer;
+
 @Component
 public class MyPropsRunner implements ApplicationRunner {
     @Override
@@ -17,7 +19,16 @@ public class MyPropsRunner implements ApplicationRunner {
             System.out.println("아규먼트 이름 = " + argName);
         }
 
-        
+        //args.getOptionNames()의 리턴타입 Set<String>
+        //Iterable의 forEach 메서드를 사용하여 모든 이름 출력
+        //Consumer의 추상메서드 accept()를 구현하는 람다식 사용
+        //1. 익명의 Inner Class (Annonymous Inner Class)
+        args.getOptionNames().forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println("Inner Class 아규먼트 이름 = " + s);
+            }
+        });
 
     }
 }
