78a1b5c37cd1a6e73684ba097ff46769d4a416b5
Seungjun Song <gunmagician@naver.com>
2025-08-13 10:55:54 +0900

    Create TestConfig.java

---
+++ Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/config/TestConfig.java
@@ -0,0 +1,17 @@
+package com.rookies4.myspringboot.config;
+
+import com.rookies4.myspringboot.config.vo.CustomVO;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class TestConfig {
+    @Bean
+    public CustomVO customVO(){
+        return CustomVO.builder() //CustomV0Builder
+                .mode("테스트모드")
+                .rate(0.5)
+                .build(); //CustomV0
+    }
+}
