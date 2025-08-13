bc4fda68b8582b872c19feeb1b305890a962f647
Seungjun Song <gunmagician@naver.com>
2025-08-13 10:59:49 +0900

    profile

--- Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/config/TestConfig.java
+++ Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/config/TestConfig.java
@@ -3,8 +3,9 @@ package com.rookies4.myspringboot.config;
 import com.rookies4.myspringboot.config.vo.CustomVO;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
-
+@Profile("test") //프로파일이 test인 경우에만 활성화
 @Configuration
 public class TestConfig {
     @Bean
