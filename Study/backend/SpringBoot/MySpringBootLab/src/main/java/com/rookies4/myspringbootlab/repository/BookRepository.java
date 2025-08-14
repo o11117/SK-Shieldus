79d1789a80a8425b6f8b5169ff1a9cb33f754a0b
Seungjun Song <gunmagician@naver.com>
2025-08-14 19:19:46 +0900

    SpringBoot HW2

--- Study/backend/SpringBoot/MySpringBootLab/src/main/java/com/rookies4/myspringbootlab/repository/BookRepository.java
+++ Study/backend/SpringBoot/MySpringBootLab/src/main/java/com/rookies4/myspringbootlab/repository/BookRepository.java
@@ -3,6 +3,7 @@ package com.rookies4.myspringbootlab.repository;
 import com.rookies4.myspringbootlab.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
@@ -11,5 +12,5 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     Optional<Book> findByIsbn(String isbn);
 
-    Optional<Book> findByAuthor(String author);
+    List<Book> findByAuthor(String author);
 }
--- Study/backend/SpringBoot/MySpringBootLab/src/test/java/com/rookies4/myspringbootlab/repository/BookRepositoryTest.java
+++ Study/backend/SpringBoot/MySpringBootLab/src/test/java/com/rookies4/myspringbootlab/repository/BookRepositoryTest.java
@@ -1,14 +1,22 @@
 package com.rookies4.myspringbootlab.repository;
 
 import com.rookies4.myspringbootlab.entity.Book;
+import org.hibernate.annotations.DynamicUpdate;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
+@Transactional
+@DynamicUpdate
 //@RequiredArgsConstructor
 public class BookRepositoryTest {
 
@@ -18,19 +26,41 @@ public class BookRepositoryTest {
     private BookRepository bookRepository;
 
     @Test
+    @Rollback(value = false)
     public void testCreateBook(){
-        Book book = new Book("스프링 부트 입문", "홍길동", "9788956746425", LocalDate.parse("2025-01-01"), 30000);
 
-        Book save = bookRepository.save(book);
-        System.out.println("save = " + save);
+        Book book = new Book();
+        book.setTitle("스프링 부트 입문");
+        book.setAuthor("홍길동");
+        book.setIsbn("9788956746425");
+        book.setPrice(30000);
+        book.setPublishDate(LocalDate.parse("2025-05-07"));
+
+        Book savedBook = bookRepository.save(book);
+        System.out.println("savedBook = " + savedBook);
     }
 
     @Test
     public void testFindByIsbn() {
-        Book book = new Book();
 
-        Optional<Book> isbn = bookRepository.findByIsbn(book.getIsbn());
-
-        System.out.println(isbn);
+        Optional<Book> bookByIsbn = bookRepository.findByIsbn("9788956746425");
+        if(bookByIsbn.isPresent()){
+        Book existingBook = bookByIsbn.get();
+        assertThat(existingBook.getIsbn()).isEqualTo("9788956746425");
+        }
     }
+    
+    @Test
+    public void testFindByAuthor() {
+        List<Book> booksByAuthor = bookRepository.findByAuthor("홍길동");
+        if(booksByAuthor.isEmpty()){
+            System.out.println("No books found for the author.");
+        }
+        assertThat(booksByAuthor).isNotEmpty();
+        for (Book book : booksByAuthor) {
+            System.out.println("Book found: " + book.getTitle() + " by " + book.getAuthor());
+        }
+
+    }
+
 }
