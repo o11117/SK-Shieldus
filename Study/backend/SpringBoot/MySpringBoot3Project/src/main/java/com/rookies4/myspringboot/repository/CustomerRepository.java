5c295b0445a54e43689dfdae3b5839f9665c0ec8
Seungjun Song <gunmagician@naver.com>
2025-08-14 10:00:04 +0900

    Update CustomerRepository.java

--- Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/repository/CustomerRepository.java
+++ Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/repository/CustomerRepository.java
@@ -3,6 +3,12 @@ package com.rookies4.myspringboot.repository;
 import com.rookies4.myspringboot.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
-    
+    //customerId로 조회하는 Query 메소드 정의
+    Optional<Customer> findByCustomerId(String customerId);
+    //customerName으로 조회하는 Query 메소드 정의
+    List<Customer> findByCustomerName(String customerName);
 }
