45f023f48965addde78a612bd5bc5733b78af334
Seungjun Song <gunmagician@naver.com>
2025-08-14 17:13:16 +0900

    Update UserRestController.java

--- Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/controller/UserRestController.java
+++ Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/controller/UserRestController.java
@@ -3,10 +3,10 @@ package com.rookies4.myspringboot.controller;
 import com.rookies4.myspringboot.entity.UserEntity;
 import com.rookies4.myspringboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,14 +19,21 @@ public class UserRestController {
 //        System.out.println("생성자 인젝션 " + userRepository.getClass().getName());
 //        this.userRepository = userRepository;
 //    }
-
+    //등록
     @PostMapping
     public UserEntity create(@RequestBody UserEntity user){
         return userRepository.save(user);
     }
-
+    //전체목록 조회
     @GetMapping
     public List<UserEntity> getUsers() {
         return userRepository.findAll();
+    }
+    //ID로 조회
+    @GetMapping("/{id}")
+    public UserEntity getUser(@PathVariable Long id) {
+        Optional<UserEntity> optionalUser = userRepository.findById(id);
+        UserEntity existUser = optionalUser.orElseThrow();
+        return existUser;
     }
 }
