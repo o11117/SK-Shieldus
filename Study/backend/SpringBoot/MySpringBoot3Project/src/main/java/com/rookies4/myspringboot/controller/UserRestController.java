f282f85f712e00e0c80758ecec38fb91e00118d3
Seungjun Song <gunmagician@naver.com>
2025-08-14 16:35:47 +0900

    Update UserRestController.java

--- Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/controller/UserRestController.java
+++ Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/controller/UserRestController.java
@@ -1,12 +1,17 @@
 package com.rookies4.myspringboot.controller;
 
+import com.rookies4.myspringboot.entity.UserEntity;
 import com.rookies4.myspringboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/users")
 public class UserRestController {
     private final UserRepository userRepository;
 
@@ -15,4 +20,9 @@ public class UserRestController {
 //        System.out.println("생성자 인젝션 " + userRepository.getClass().getName());
 //        this.userRepository = userRepository;
 //    }
+
+    @PostMapping
+    public UserEntity create(@RequestBody UserEntity user){
+        return userRepository.save(user);
+    }
 }
