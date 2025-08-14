ece0f7fefdf318807c133ed6f3b7c4dbe8d6e670
Seungjun Song <gunmagician@naver.com>
2025-08-14 16:04:52 +0900

    Create UserRestController.java

---
+++ Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/controller/UserRestController.java
@@ -0,0 +1,16 @@
+package com.rookies4.myspringboot.controller;
+
+import com.rookies4.myspringboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserRestController {
+    private UserRepository userRepository;;
+
+    //Constructor injection
+    public UserRestController(UserRepository userRepository) {
+        System.out.println("생성자 인젝션 " + userRepository.getClass().getName());
+        this.userRepository = userRepository;
+    }
+}
