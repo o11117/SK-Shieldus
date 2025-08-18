940d83bdb81c33f05778922dafef481837cc7038
Seungjun Song <gunmagician@naver.com>
2025-08-18 17:35:49 +0900

    error

--- Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/controller/UserController.java
+++ Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -35,6 +36,23 @@ public class UserController {
         userRepository.save(user);
         //model.addAttribute("users", userRepository.findAll());
         //return "index";
+        return "redirect:/users/index";
+    }
+
+    @GetMapping("/users/edit/{id}")
+        public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        UserEntity user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        model.addAttribute("user", user);
+        return "update-user";
+    }
+
+    @PostMapping("/users/update/{id}")
+        public String updateUser(@PathVariable("id") long id, @Valid @ModelAttribute("user") UserEntity user, BindingResult result) {
+        if (result.hasErrors()) {
+        user.setId(id);
+        return "update-user";
+        }
+        userRepository.save(user);
         return "redirect:/users/index";
     }
 
