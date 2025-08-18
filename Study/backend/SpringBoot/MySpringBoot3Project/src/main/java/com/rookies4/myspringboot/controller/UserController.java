b1dd66ec678c602c7abed6fe87f39423dd890166
Seungjun Song <gunmagician@naver.com>
2025-08-18 14:15:28 +0900

    thymeleaf

---
+++ Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.rookies4.myspringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/thymeleaf")
+    public String leaf(Model model) {
+        model.addAttribute("name","basic");
+        return "leaf";
+    }
+}
