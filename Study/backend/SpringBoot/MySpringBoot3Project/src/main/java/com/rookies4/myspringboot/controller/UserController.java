7920e1ac357ad22e60b9c34af5d723ae8cb4d444
Seungjun Song <gunmagician@naver.com>
2025-08-18 16:31:47 +0900

    Update UserController.java

--- Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/controller/UserController.java
+++ Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/controller/UserController.java
@@ -2,11 +2,14 @@ package com.rookies4.myspringboot.controller;
 
 import com.rookies4.myspringboot.entity.UserEntity;
 import com.rookies4.myspringboot.repository.UserRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -18,18 +21,32 @@ public class UserController {
 
     @GetMapping("/users/signup")
     public String showSignUpForm(@ModelAttribute("user") UserEntity user) {
-    return "add-user";
+        return "add-user";
     }
 
-    @GetMapping("users/index")
+    @PostMapping("/users/adduser")
+    public String addUser(@Valid @ModelAttribute("user") UserEntity user,
+                          BindingResult result, Model model) {
+        //검증 오류가 발생한 경우
+        if (result.hasErrors()) {
+            return "add-user";
+        }
+        //등록
+        userRepository.save(user);
+        model.addAttribute("users", userRepository.findAll());
+        return "index";
+        //return "redirect:/index";
+    }
+
+    @GetMapping("/users/index")
     public ModelAndView index() {
         List<UserEntity> userEntityList = userRepository.findAll();
-        return new ModelAndView("index", "users", userEntityList);
+        return new ModelAndView("index","users",userEntityList);
     }
 
     @GetMapping("/thymeleaf")
     public String leaf(Model model) {
-        model.addAttribute("name","스프링부트");
+        model.addAttribute("name", "스프링부트");
         return "leaf";
     }
 }
