e22d48a22dbc0f42874e9d849b95bd8d15fb179e
Seungjun Song <gunmagician@naver.com>
2025-08-19 14:10:38 +0900

    security

--- Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/security/config/SecurityConfig.java
+++ Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/security/config/SecurityConfig.java
@@ -3,8 +3,12 @@ package com.rookies4.myspringboot.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -13,4 +17,19 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+        //authentication
+        public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+            UserDetails admin = User.withUsername("adminboot")
+                    .password(encoder.encode("pwd1"))
+                    .roles("ADMIN")
+                    .build();
+            UserDetails user = User.withUsername("userboot")
+                    .password(encoder.encode("pwd2"))
+                    .roles("USER")
+                    .build();
+            return new InMemoryUserDetailsManager(admin, user);
+        }
+
 }
