3daa1f79f89c4a80e4df9d8843123aa6bad34ee1
Seungjun Song <gunmagician@naver.com>
2025-08-19 16:04:09 +0900

    Update UserInfoUserDetails.java

--- Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/security/models/UserInfoUserDetails.java
+++ Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/security/models/UserInfoUserDetails.java
@@ -16,6 +16,7 @@ public class UserInfoUserDetails implements UserDetails {
     private List<GrantedAuthority> authorities;
     private UserInfo userInfo;
 
+    //생성자로 UserInfo 엔티티 객체를 주입 받는다.
     public UserInfoUserDetails(UserInfo userInfo) {
         this.userInfo = userInfo;
         this.email=userInfo.getEmail();
@@ -29,6 +30,10 @@ public class UserInfoUserDetails implements UserDetails {
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return authorities;
     }
+
+    /*
+        AuthenticationManager객체가 인증을 처리할 때 getPassword()와 getUsername() 메소드를 호출한다.
+     */
 
     @Override
     public String getPassword() {
