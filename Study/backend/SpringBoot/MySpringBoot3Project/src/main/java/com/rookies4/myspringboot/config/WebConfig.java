fc14c54b95dc73032a5230a856b4639de0704709
Seungjun Song <gunmagician@naver.com>
2025-08-19 09:55:35 +0900

    cors

--- Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/config/WebConfig.java
+++ Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.rookies4.myspringboot.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,5 +14,12 @@ public class WebConfig implements WebMvcConfigurer {
                 //반드시 mobile 다음에 / 을 주어야 한다.
                 .addResourceLocations("classpath:/mobile/")
                 .setCachePeriod(20);//20초
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
     }
 }
