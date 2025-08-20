5445eef6690fb83d7fb19c6ebe42b0146cfd716b
Seungjun Song <gunmagician@naver.com>
2025-08-20 13:13:18 +0900

    openai

--- Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/service/UserService.java
+++ Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/service/UserService.java
@@ -1,8 +1,11 @@
 package com.rookies4.myspringboot.service;
 
 import com.rookies4.myspringboot.controller.dto.UserDTO;
+import com.rookies4.myspringboot.entity.UserEntity;
+import com.rookies4.myspringboot.exception.BusinessException;
 import com.rookies4.myspringboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +17,13 @@ public class UserService {
 
     @Transactional
     public UserDTO.UserResponse createUser(UserDTO.UserCreateRequest request){
-        
+        userRepository.findByEmail(request.getEmail())
+                .ifPresent(user -> {
+                    throw new BusinessException("User with this email already exists", HttpStatus.CONFLICT);
+                });
+        //DTO를 Entity로 변환하여 저장
+        UserEntity entity = request.toEntity();
+        UserEntity savedEntity = userRepository.save(entity);
+        return new UserDTO.UserResponse(savedEntity);
     }
 }
