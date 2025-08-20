68ae8f4e576289085749b72d68a25de3ccc0679f
Seungjun Song <gunmagician@naver.com>
2025-08-20 13:43:09 +0900

    Update UserServiceRestController.java

--- Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/controller/UserServiceRestController.java
+++ Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/controller/UserServiceRestController.java
@@ -5,10 +5,9 @@ import com.rookies4.myspringboot.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,5 +20,11 @@ public class UserServiceRestController {
             @Valid @RequestBody UserDTO.UserCreateRequest request) {
         UserDTO.UserResponse createdUser = userService.createUser(request);
         return ResponseEntity.ok(createdUser);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO.UserResponse> getUserById(@PathVariable Long id) {
+        UserDTO.UserResponse userById = UserService.getUserById(id);
+        return ResponseEntity.ok(userById);
     }
 }
