d279754d02abd07ea7652daabd4b5ba82643a43d
Seungjun Song <gunmagician@naver.com>
2025-08-20 10:43:06 +0900

    Update UserService.java

--- Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/service/UserService.java
+++ Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/service/UserService.java
@@ -1,12 +1,19 @@
 package com.rookies4.myspringboot.service;
 
+import com.rookies4.myspringboot.controller.dto.UserDTO;
 import com.rookies4.myspringboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
     private final UserRepository userRepository;
-    
+
+    @Transactional
+    public UserDTO.UserResponse createUser(UserDTO.UserCreateRequest request){
+        
+    }
 }
