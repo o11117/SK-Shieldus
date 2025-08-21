a6c2912cb547717e5fc0cbdd349b52047bb89881
Seungjun Song <gunmagician@naver.com>
2025-08-21 14:25:30 +0900

    Reapply "Update StudentDTO.java"
    This reverts commit 6390abef5d6724660cc1be6e600e2c368b5692a9.

--- Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/controller/dto/StudentDTO.java
+++ Study/backend/SpringBoot/MySpringBoot3Project/src/main/java/com/rookies4/myspringboot/controller/dto/StudentDTO.java
@@ -43,7 +43,8 @@ public class StudentDTO {
         @NotBlank(message = "Phone number is required")
         @Size(max = 20, message = "Phone number cannot exceed 20 characters")
         private String phoneNumber;
-        
+
+        @NotBlank(message = "Email is required")
         @Size(max = 100, message = "Email cannot exceed 100 characters")
         private String email;
         
@@ -60,6 +61,7 @@ public class StudentDTO {
         private String studentNumber;
         private StudentDetailResponse detail;
 
+        //Entity to DTO conversion method
         public static Response fromEntity(Student student) {
             StudentDetailResponse detailResponse = student.getStudentDetail() != null
                     ? StudentDetailResponse.builder()
