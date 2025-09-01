564d5c67399cc7f7c572a7aec579414aede7c447
Seungjun Song <gunmagician@naver.com>
2025-09-01 10:57:39 +0900

    add MyComponent for study

--- Study/frontend/2reactjs/reacthello-vite/src/components/MyComponent.jsx
+++ Study/frontend/2reactjs/reacthello-vite/src/components/MyComponent.jsx
@@ -6,17 +6,23 @@ class MyComponent extends Component {
     state = {
         value : 0,
     }
-
+    //event handler
+    handleDecrement = () => {
+        this.setState({value: this.state.value - 1});
+    }
     render() {
         //destructuring assignment
         const {name, age} = this.props;
         const {value} = this.state;
+        const {handleDecrement} = this;
 
         return (
             <div>
                 <h2>클래스 타입 컴포넌트</h2>
                 <h3>Hello!{name} - {age}</h3>
                 <p>상태변수 value = {value}</p>
+                <button onClick={() => this.setState({value: value + 1})}>증가</button>
+                <button onClick={handleDecrement}>감소</button>    
             </div>
         );
     }
