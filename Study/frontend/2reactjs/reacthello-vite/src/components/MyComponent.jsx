2516b6be87923bf6b5233710b6033f17f64bec17
Seungjun Song <gunmagician@naver.com>
2025-09-01 10:31:30 +0900

    add MyComponent for study

--- Study/frontend/2reactjs/reacthello-vite/src/components/MyComponent.jsx
+++ Study/frontend/2reactjs/reacthello-vite/src/components/MyComponent.jsx
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class MyComponent extends Component {
+    //컴포넌트 내부에 상태변수를 포함하는 state객체 선언
+    state = {
+        value : 0,
+    }
+
     render() {
         //destructuring assignment
         const {name, age} = this.props;
+        const {value} = this.state;
 
         return (
             <div>
                 <h2>클래스 타입 컴포넌트</h2>
                 <h3>Hello!{name} - {age}</h3>
+                <p>상태변수 value = {value}</p>
             </div>
         );
     }
@@ -21,4 +28,5 @@ MyComponent.propTypes = {
     name: PropTypes.string,
     age: PropTypes.number.isRequired
 };
+
 export default MyComponent;
