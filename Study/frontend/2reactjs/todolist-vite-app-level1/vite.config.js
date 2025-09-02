cce6c9ea6eafd259837e5659acdb2ce9d03421e7
Seungjun Song <gunmagician@naver.com>
2025-09-02 16:41:49 +0900

    config

--- Study/frontend/2reactjs/todolist-vite-app-level1/vite.config.js
+++ Study/frontend/2reactjs/todolist-vite-app-level1/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, r } from 'vite';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000
+  },
+  resolve: {
+    alias: [
+      { find: "@", replacement: '/src' },
+      { find: "@components", replacement: '/src/components' }
+    ]
 })
