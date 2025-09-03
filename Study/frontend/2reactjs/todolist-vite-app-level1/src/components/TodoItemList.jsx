afa94bc4da9c1b47b80205256f4d83a0b71a7a48
Seungjun Song <gunmagician@naver.com>
2025-09-03 13:06:01 +0900

    todo

--- Study/frontend/2reactjs/todolist-vite-app-level1/src/components/TodoItemList.jsx
+++ Study/frontend/2reactjs/todolist-vite-app-level1/src/components/TodoItemList.jsx
@@ -9,20 +9,24 @@ class TodoItemList extends Component {
     componentDidMount() {
         this.props.getTodos();
     }
-    //true 리턴 (myTodos 변수에 변경이 있는 경우)면 render() 호출
-    //false 리턴 (myTodos 변수에 변경이 없는 경우)면 render() 호출 안함
+    /*
+        true 리턴 (myTodos 변수에 변동이 있는 경우)이면 render() 함수가 호출됨
+        false 리턴 (myTodos 변수에 변동이 없는 경우)이면 render() 함수가 호출되지 않음(렌더링 생략)
+    */
     shouldComponentUpdate(nextProps, nextState) {
         return this.props.myTodos !== nextProps.myTodos;
     }
 
     render() {
         const { myTodos } = this.props;
-        const todoList = myTodos.map(({id, text, checked}) => (
-            <TodoItem
-                key={id}
-                id={id}
+        /*
+           const { id,text,checked } = todo;
+        */
+        const todoList = myTodos.map(({ id, text, checked }) => (
+            <TodoItem id={id}
                 text={text}
                 checked={checked}
+                key={id}
             />
         ));
         return (
@@ -32,14 +36,14 @@ class TodoItemList extends Component {
         );
     }
 }
+
 TodoItemList.propTypes = {
     myTodos: PropTypes.array,
-    getTodos: PropTypes.func
+    getTodos: PropTypes.func,
 };
-
 export default connect(
-    //store에 저장된 todos를 가져와서 myTodos 프로퍼티에 매핑
-    (state) => ({ myTodos: state.todos }),
-    //action함수를 dispatch 하는 함수를 getTodos 프로퍼티에 매핑
-    { getTodos: fetchAllTodos }
+    //store에 저장된 todos를 가져와서 myTodos 프로퍼티에 매핑하기
+    (state) => ({myTodos: state.todos}),
+    //action함수를 dispatch 하는 함수를 getTodos 프로퍼티에 매핑하기
+    { getTodos: fetchAllTodos } // fetchAllTodos 프로터티에 매핑한다면 { fetchAllTodos }
 )(TodoItemList);
