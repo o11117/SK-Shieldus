51d9b80bfc46f663d0b786deeffcd5cae1cc5ed8
Seungjun Song <gunmagician@naver.com>
2025-09-03 10:43:17 +0900

    Edit Form

--- Study/frontend/2reactjs/todolist-vite-app-level1/src/App.jsx
+++ Study/frontend/2reactjs/todolist-vite-app-level1/src/App.jsx
@@ -1,7 +1,7 @@
 import { Component } from 'react';
-import Form from './components/Form';
-import TodoItemList from './components/TodoItemList';
-import TodoListTemplate from './components/TodoListTemplate';
+import Form from '@components/Form';
+import TodoItemList from '@components/TodoItemList';
+import TodoListTemplate from '@components/TodoListTemplate';
 
 
 class App extends Component {
